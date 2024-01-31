3daef6a3c41588eb48327e11537467f309c30cf4
Thiago Marques <thiagomarquessilva99@gmail.com>
2024-01-31 10:44:53 -0300

    Inicialização do Spring Boot

---
+++ libras/src/main/java/com/portal/libras/LibrasApplication.java
@@ -0,0 +1,13 @@
+package com.portal.libras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrasApplication.class, args);
+	}
+
+}
---
+++ libras/src/test/java/com/portal/libras/LibrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.libras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
