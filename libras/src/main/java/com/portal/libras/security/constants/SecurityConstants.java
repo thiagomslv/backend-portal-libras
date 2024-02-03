91d7d29ecaecba9afb6b039b7a221abb93410ddd
Thiago Marques <thiagomarquessilva99@gmail.com>
2024-02-03 10:15:59 -0300

    Implementação do filtro de geração do token JWT

---
+++ libras/src/main/java/com/portal/libras/security/constants/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.portal.libras.security.constants;
+
+public interface SecurityConstants {
+
+    public static final String JWT_KEY = "550a8decf8fa466e27b02e7628cf59ee";
+    public static final String JWT_HEADER = "Authorization";
+    public static final String AUTH_ROUTE = "/auth";
+}
---
+++ libras/src/main/java/com/portal/libras/security/filters/JWTTokenGeneratorFilter.java
@@ -0,0 +1,57 @@
+package com.portal.libras.security.filters;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.portal.libras.security.constants.SecurityConstants;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTTokenGeneratorFilter extends OncePerRequestFilter{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        /*  Se o usuário não estiver autenticado, gera o token JWT. Esse filtro só será executado na rota de login,
+            por isso, não há risco de se gerar um token para um usuário não autenticado, uma vez que o AuthenticationProvider
+            gera uma exceção em caso de erro.
+        */
+        if(authentication != null){
+
+            //Gera a chave de assinatura do token JWT.
+            SecretKey key = Keys.hmacShaKeyFor(SecurityConstants.JWT_KEY.getBytes(StandardCharsets.UTF_8));
+
+            String jwtToken = Jwts.builder().claim("email", authentication.getName())
+                                            .issuedAt(new Date())
+                                            .expiration(new Date(new Date().getTime() + 30000000))
+                                            .signWith(key).compact();
+
+            response.setHeader(SecurityConstants.JWT_HEADER, jwtToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request){
+
+        //Só executa o filtro na rota de autenticação.
+        return !request.getServletPath().equals(SecurityConstants.AUTH_ROUTE);
+    }
+    
+}
